13d8e077a00b0d0cf628a4e9fbcc4915cd46c365
divz2711 <divyasampathc@gmail.com>
2024-08-24 12:31:02 +0530

    first commit~

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask,request,render_template
+import numpy as np
+import pandas
+import sklearn
+import pickle
+import joblib
+
+# importing model
+model = joblib.load('svmMODEL.h5')
+
+# creating flask app
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route("/predict",methods=['POST'])
+def predict():
+    N = request.form['Nitrogen']
+    P = request.form['Phosporus']
+    K = request.form['Potassium']
+    temp = request.form['Temperature']
+    humidity = request.form['Humidity']
+    ph = request.form['Ph']
+    rainfall = request.form['Rainfall']
+
+    feature_list = [N, P, K, temp, humidity, ph, rainfall]
+    single_pred = np.array(feature_list).reshape(1, -1)
+
+    prediction = model.predict(single_pred)
+
+    crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
+                 8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
+                 14: "Pomegranate", 15: "Lentil", 16: "Blackgram", 17: "Mungbean", 18: "Mothbeans",
+                 19: "Pigeonpeas", 20: "Kidneybeans", 21: "Chickpea", 22: "Coffee",23 : "error"}
+
+    
+    result = "{} is the best crop to be cultivated right there".format(prediction)
+   
+    return render_template('index.html',result = result)
+
+
+
+
+# python main
+if __name__ =='__main__':
+	app.run(host='0.0.0.0',port=8080)
